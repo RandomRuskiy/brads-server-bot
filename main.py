7577a482e9763485227cc5f9f9a8042c08ff62fa
RandomRuskiy <jbotterall@gmail.com>
2021-08-09 12:07:59 +0100

    fix typo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
-from discord_slash import SlashCommand. SlashContext
+from discord_slash import SlashCommand, SlashContext
 import os
 import logging
 from pathlib import Path
