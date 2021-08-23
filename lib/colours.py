df242dcba058704315d23839cf8520d3fbf745f0
RandomRuskiy <jbotterall@gmail.com>
2021-08-23 11:11:08 +0100

    added colours from discord roles into colours.py

--- lib/colours.py
+++ lib/colours.py
@@ -1,3 +1,17 @@
+'''
+    Dictionary varibles that can be imported for ease of use in embeds etc.
+
+    To import:
+        "from lib.colours import BasicColours (or) RoleColours"
+
+        When setting a colour in an embed, where color = *, you can do BasicColours/RoleColours["colour"] instead of manually setting the hex value.
+
+        Basic Colours are just basic solid colours like its said in the name
+
+        Role Colours are the same colours as the colour roles in the discord which can help you select a colour according to that or something
+'''
+
+
 BasicColours = {
     "red": 0xE3242B,
     "orange": 0xFC6A03,
@@ -11,4 +25,21 @@ BasicColours = {
     "grey": 0x59515E,
     "white": 0xFFFFFF,
     "cyan": 0x26fff1
+}
+
+RoleColours = {
+    "dark_red": 0xb30000,
+    "red": 0xff0000,
+    "light_red": 0xff5c5c,
+    "orange": 0xe67e22,
+    "green": 0x0dac30,
+    "dark_green": 0x026400,
+    "dark_blue": 0x00568d,
+    "blue": 0x3498db,
+    "light_blue": 0x6dc2f9,
+    "lavender": 0x7885ff,
+    "purple": 0x6e00ff,
+    "lilac": 0x8759b6,
+    "magenta": 0xf674fd,
+    "pink": 0xf800ff
 }
