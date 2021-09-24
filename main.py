5d8610806209f4d397525afd44ea4051cec88749
RandomRuskiy <jbotterall@gmail.com>
2021-09-24 13:27:29 +0100

    add more cmds

--- main.py
+++ main.py
@@ -129,6 +129,122 @@ class Commands():
             )
             pass
 
+    @client.slash_command(
+        guild_ids=guild_ids
+    )
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, user: discord.Member = None, *, reason=None):
+        if user is None or user == ctx.author:
+            await ctx.send("lol you cant ban yourself")
+            return
+
+        elif user == self.bot.user:
+            await ctx.send("why you try ban me :(")
+            return
+
+        elif reason is None:
+            reason = "No reson specified"
+        message = f"You have been banned from **{ctx.guild.name}** for **{reason}**"
+        await ctx.guild.ban(user, reason=reason)
+        await ctx.send(f"**{user}** has been banned!")
+
+    @client.slash_command(
+        guild_ids=guild_ids
+    )
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f"**{user}** has been unbanned!")
+            return
+
+    @client.slash_command(
+        guild_ids=guild_ids
+    )
+    @commands.has_permissions(ban_members=True)
+    async def kick(self, ctx, user: discord.Member, reason=None):
+        if user is None or user == ctx.author:
+            await ctx.send('You cant kick yourself lol')
+            return
+
+        elif user == self.bot.user:
+            await ctx.send('yo im not kicking myself')
+            return
+
+        elif reason is None:
+            reason = 'No reason specified'
+
+        await ctx.guild.kick(user, reason=reason)
+        await ctx.send(f'**{user}** has been kicked!')
+
+    @client.slash_command(
+        guild_ids=guild_ids
+    )
+    @commands.has_permissions(manage_messages=True)
+    async def mute(self, ctx, user: discord.Member, time=None, reason=None):
+        mute_role = discord.utils.get(user.roles, id=879041072486035506)
+        if user is None or user == ctx.author:
+            await ctx.send('You cant mute yourself')
+            return
+        elif mute_role is True:
+            await ctx.send('That user is already muted')
+
+        elif time is None:
+            await ctx.send('You need to specify a time. e.g. 1s, 1m, 1h, 1d')
+            return
+
+        else:
+            if not reason:
+                'No reason specified'
+
+            try:
+                seconds = time[:-1]
+                duration = time[-1]
+                if duration == 's':
+                    seconds = seconds * 1
+                elif duration == 'm':
+                    seconds = seconds * 60
+                elif duration == 'h':
+                    seconds = seconds * 60 * 60
+                elif duration == 'd':
+                    seconds = seconds * 86400
+
+                else:
+                    ctx.send('put a correct duration smh')
+                    return
+
+            except Exception as e:
+                print(e)
+                await ctx.send('error happened, prob invalid time input or something idk')
+                return
+            await user.add_roles(mute_role, reason=reason)
+            await ctx.send(f'Muted {user} for reason: {reason}')
+            await asyncio.sleep(seconds)
+            await user.remove_roles(mute_role, reason='Duration of mute over')
+
+    @client.slash_command(
+        guild_ids=guild_ids
+    )
+    @commands.has_permissions(manage_messages=True)
+    async def unmute(self, ctx, user: discord.Member, reason=None):
+        mute_role = discord.utils.get(user.roles, id=879041072486035506)
+        if user is None or user == ctx.author:
+            await ctx.send('how')
+            return
+        elif reason is None:
+            reason = 'No reason specified'
+        elif not mute_role:
+            await ctx.send('That user isnt muted smh')
+        else:
+            await user.remove_roles(mute_role, reason='Mute manually removed')
+            await ctx.send(f'Unmuted {user}')
+
 
 # keep this at bottom
 
