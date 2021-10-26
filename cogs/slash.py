efc7a2cb259e346ae9240991a1c955088f0d4f38
RandomRuskiy <jbotterall@gmail.com>
2021-10-26 16:45:35 +0100

    last set status is now written to file

--- cogs/slash.py
+++ cogs/slash.py
@@ -52,9 +52,12 @@ class Slash(commands.Cog):
     @commands.cooldown(rate=1, per=30)
     async def setstatusslash(self, ctx, *, text: str):
         activity = discord.Game(name=text)
+        act = open("../data/laststatus", "rw")
+        act.write(text)
         await self.bot.change_presence(status=discord.Status.online,
                                        activity=activity)
         await ctx.send(f'yo my status is now **"{text}"**')
+        act.close()
 
     @client.slash_command(
         guild_ids=guild_ids
