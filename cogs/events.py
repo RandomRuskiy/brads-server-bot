ae488e63e4f672a73e0fb71e83c6729af11d4d47
RandomRuskiy <jbotterall@gmail.com>
2022-02-22 23:59:18 +0000

    bug

--- cogs/events.py
+++ cogs/events.py
@@ -167,7 +167,7 @@ class Events(commands.Cog):
 
                 elif message.content.lower() == 'mama':
                     await message.channel.send('yo mama large hahahahahhahahahahahahahhahah got you there!!!!lolololololo')
-                    asyncio.sleep(5)
+                    await asyncio.sleep(5)
                     await message.channel.send('lolololololololololo stll laughing at that joke i made about your mother hahahhahahahahahhahahahahahahha')
                     await message.channel.send('https://tenor.com/view/ronaldo-gif-24433817')
 
