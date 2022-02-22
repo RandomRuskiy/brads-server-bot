4cdff86520ce92e4e68b0a4e1e3fc469a2af4f88
RandomRuskiy <jbotterall@gmail.com>
2022-02-22 23:56:04 +0000

    bug

--- cogs/events.py
+++ cogs/events.py
@@ -166,10 +166,10 @@ class Events(commands.Cog):
                     await message.channel.send('(real)')
 
                 elif message.content.lower() == 'mama':
-                    message.channel.respond('yo mama large hahahahahhahahahahahahahhahah got you there!!!!lolololololo')
+                    message.channel.send('yo mama large hahahahahhahahahahahahahhahah got you there!!!!lolololololo')
                     asyncio.sleep(5)
-                    message.channel.respond('lolololololololololo stll laughing at that joke i made about your mother hahahhahahahahahhahahahahahahha')
-                    message.channel.respond('https://tenor.com/view/ronaldo-gif-24433817')
+                    message.channel.send('lolololololololololo stll laughing at that joke i made about your mother hahahhahahahahahhahahahahahahha')
+                    message.channel.send('https://tenor.com/view/ronaldo-gif-24433817')
 
         if is_bot(message) is False:
 
